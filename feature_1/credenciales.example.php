b2175cc76cb5ccc3e7d147ea2d374e3a55671636
Romansaurius <ranuccimanro07@gmail.com>
2025-10-20 18:41:24 -0300

    Delete feature_1 directory

--- feature_1/credenciales.example.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Ejemplo de credenciales - Copiar a credenciales.php y completar
-$servidor = "tu_servidor_mysql";
-$usuario = "tu_usuario";
-$password = "tu_password";
-$baseDatos = "burgerkong";
-$puerto = 3306;
-?>
--- feature_1/sql/categorias.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO BurgerKong__categorias (id, nombre, fechaCreacion) VALUES ('28', 'Acompañamientos', '2022-08-12 10:57:24');
-INSERT INTO BurgerKong__categorias (id, nombre, fechaCreacion) VALUES ('33', 'Bondiolita King', '2022-08-12 10:57:24');
-INSERT INTO BurgerKong__categorias (id, nombre, fechaCreacion) VALUES ('25', 'Carne a la parrilla', '2022-08-12 10:57:24');
-INSERT INTO BurgerKong__categorias (id, nombre, fechaCreacion) VALUES ('26', 'De Pollo', '2022-08-12 10:57:24');
-INSERT INTO BurgerKong__categorias (id, nombre, fechaCreacion) VALUES ('29', 'Desayunos y Meriendas', '2022-08-12 10:57:24');
-INSERT INTO BurgerKong__categorias (id, nombre, fechaCreacion) VALUES ('27', 'Ensaladas', '2022-08-12 10:57:24');
-INSERT INTO BurgerKong__categorias (id, nombre, fechaCreacion) VALUES ('30', 'Niños', '2022-08-12 10:57:24');
-INSERT INTO BurgerKong__categorias (id, nombre, fechaCreacion) VALUES ('31', 'Postre', '2022-08-12 10:57:24');
-INSERT INTO BurgerKong__categorias (id, nombre, fechaCreacion) VALUES ('32', 'Vegetal', '2022-08-12 10:57:24');
