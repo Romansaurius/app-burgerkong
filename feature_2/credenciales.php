1f5f78579803ff1dbd3ca24125fe3b85c779500a
Romansaurius <ranuccimanro07@gmail.com>
2025-10-20 19:06:00 -0300

    cambio de puerto

--- feature_2/credenciales.php
+++ feature_2/credenciales.php
@@ -1,13 +1,14 @@
 <?php
-	
-	//HOST => url = dominio / ip localhost = 127.0.0.1
-	//USER => usuario de la base de datos
-	//PASS => pass del usuario de la base
-	//DB => es la db que voy a utilizar
+// Credenciales Railway
+$servidor = "shuttle.proxy.rlwy.net";
+$usuario = "root";
+$password = "anJkMDnhTJoXaMDjgYFpfmkMBUskRZFu";
+$baseDatos = "BurgerKong";
+$puerto = 21840;
 
-	define("HOST", "shuttle.proxy.rlwy.net"); // esto queda fijo
-	define("USER", "root"); // coloca tu usuario
-	define("PASS", "anJkMDnhTJoXaMDjgYFpfmkMBUskRZFu"); // coloca tu pass
-	define("DB", "BurgerKong");	// coloca tu base
-
- ?>
+// Compatibilidad con código antiguo
+define("HOST", $servidor);
+define("USER", $usuario);
+define("PASS", $password);
+define("DB", $baseDatos);
+?>
