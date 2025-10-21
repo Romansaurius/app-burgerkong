97dc660ac4b7e34a2bb8ff6d78992fba1e9cb1e3
Romansaurius <ranuccimanro07@gmail.com>
2025-10-21 02:18:01 -0300

    asadasdaaaaaaaaaaaaaaaaaaa

--- feature_2/credenciales.php
+++ feature_2/credenciales.php
@@ -6,9 +6,9 @@ $password = "anJkMDnhTJoXaMDjgYFpfmkMBUskRZFu";
 $baseDatos = "BurgerKong";
 $puerto = 21840;
 
-// Compatibilidad con código antiguo
-define("HOST", $servidor);
-define("USER", $usuario);
-define("PASS", $password);
-define("DB", $baseDatos);
+// Compatibilidad con código antiguo (solo si no están definidas)
+if (!defined('HOST')) define("HOST", $servidor);
+if (!defined('USER')) define("USER", $usuario);
+if (!defined('PASS')) define("PASS", $password);
+if (!defined('DB')) define("DB", $baseDatos);
 ?>
